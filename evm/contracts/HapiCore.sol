ca9aba1893e083f2ef1f56eeb1304893eea50653
Andrei Arsenin <andrei.arsenin@hapi.one>
2023-05-11 16:43:03 +0300

    Add setAuthority method

--- evm/contracts/HapiCore.sol
+++ evm/contracts/HapiCore.sol
@@ -3,11 +3,29 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
 contract HapiCore is OwnableUpgradeable {
+    address private _authority;
+
+    modifier onlyOwnerOrAuthority() {
+        require(owner() == _msgSender() || _authority == _msgSender(), "Caller is not the owner or authority");
+        _;
+    }
+
     function initialize() public initializer {
         __Ownable_init();
+
+        _authority = _msgSender();
+    }
+
+    event AuthorityChanged(address authority);
+
+    function setAuthority(address newAuthority) public onlyOwnerOrAuthority {
+        _authority = newAuthority;
+
+        emit AuthorityChanged(newAuthority);
+    }
+
+    function authority() public view virtual returns (address) {
+        return _authority;
     }
 }
--- evm/test/HapiCore.ts
+++ evm/test/HapiCore.ts
@@ -8,16 +8,52 @@ describe("HapiCore", function () {
   async function basicFixture() {
     let setup = await setupContract();
 
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner, authority, nobody] = await ethers.getSigners();
 
-    return { ...setup, owner, otherAccount };
+    return { ...setup, owner, authority, nobody };
   }
 
   describe("Deployment", function () {
-    it("Should set the right owner", async function () {
+    it("Should set the right owner and authority", async function () {
       const { hapiCore, owner } = await loadFixture(basicFixture);
 
       expect(await hapiCore.owner()).to.equal(owner.address);
+
+      expect(await hapiCore.authority()).to.equal(owner.address);
+    });
+
+    it("Should correctly set authority from owner", async function () {
+      const { hapiCore, owner, authority } = await loadFixture(basicFixture);
+
+      expect(await hapiCore.setAuthority(authority.address))
+        .to.emit(hapiCore, "AuthorityChanged")
+        .withArgs(authority.address);
+
+      expect(await hapiCore.authority()).to.equal(authority.address);
+    });
+
+    it("Should correctly set authority from previous authority", async function () {
+      const { hapiCore, authority, nobody } = await loadFixture(basicFixture);
+
+      expect(await hapiCore.setAuthority(authority.address))
+        .to.emit(hapiCore, "AuthorityChanged")
+        .withArgs(authority.address);
+
+      expect(await hapiCore.authority()).to.equal(authority.address);
+
+      expect(await hapiCore.connect(authority).setAuthority(nobody.address))
+        .to.emit(hapiCore, "AuthorityChanged")
+        .withArgs(authority.address);
+
+      expect(await hapiCore.authority()).to.equal(nobody.address);
+    });
+
+    it("Should not allow setting authority from non-owner/non-authority", async function () {
+      const { hapiCore, authority, nobody } = await loadFixture(basicFixture);
+
+      await expect(
+        hapiCore.connect(nobody).setAuthority(authority.address)
+      ).to.be.revertedWith("Caller is not the owner or authority");
     });
   });
 });
