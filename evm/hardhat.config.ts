d77b5dfd9bc12e0525a29feb878c41186ff3b25b
Andrei Arsenin <andrei.arsenin@hapi.one>
2023-05-11 13:11:08 +0300

    Setup deploy/upgrade tasks

--- evm/hardhat.config.ts
+++ evm/hardhat.config.ts
@@ -1,9 +1,71 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
 };
+
+task("deploy", "Deploys the HAPI Core contract").setAction(async (_, hre) => {
+  try {
+    let network = await hre.ethers.provider.getNetwork();
+
+    console.log(`Deploying to '${network.name}' (${network.chainId})`);
+
+    const HapiCore = await hre.ethers.getContractFactory("HapiCore");
+
+    const contract = await hre.upgrades.deployProxy(HapiCore, [], {
+      initializer: "initialize",
+    });
+
+    await contract.deployed();
+
+    const adminAddress = await hre.upgrades.erc1967.getAdminAddress(
+      contract.address
+    );
+    const implementationAddress =
+      await hre.upgrades.erc1967.getImplementationAddress(contract.address);
+
+    console.log(`HAPI Core deployed`, {
+      contract: contract.address,
+      admin: adminAddress,
+      implementation: implementationAddress,
+    });
+  } catch (error) {
+    console.error(`${error}`);
+    process.exit(1);
+  }
+});
+
+task("upgrade", "Upgrades the HAPI Core contract")
+  .addParam("address", "Contract address")
+  .setAction(async (args, hre) => {
+    try {
+      let network = await hre.ethers.provider.getNetwork();
+
+      console.log(`Deploying to '${network.name}' (${network.chainId})`);
+
+      const HapiCore = await hre.ethers.getContractFactory("HapiCore");
+
+      const contract = await hre.upgrades.upgradeProxy(args.address, HapiCore);
+
+      await contract.deployed();
+
+      const adminAddress = await hre.upgrades.erc1967.getAdminAddress(
+        contract.address
+      );
+      const implementationAddress =
+        await hre.upgrades.erc1967.getImplementationAddress(contract.address);
+
+      console.log(`HAPI Core upgraded`, {
+        contract: contract.address,
+        admin: adminAddress,
+        implementation: implementationAddress,
+      });
+    } catch (error) {
+      console.error(`${error}`);
+      process.exit(1);
+    }
+  });
 
 export default config;
