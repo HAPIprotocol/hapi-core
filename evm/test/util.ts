cd3e4292f3b08d0951f9c68ed1af86a3a0bb8cb6
Andrei Arsenin <andrei.arsenin@hapi.one>
2023-05-12 16:42:09 +0300

    Add create/update reporter

---
+++ evm/test/util.ts
@@ -0,0 +1,19 @@
+import { BigNumber } from "ethers";
+import { ethers } from "hardhat";
+
+export function randomId() {
+  return BigNumber.from(ethers.utils.randomBytes(16));
+}
+
+export enum ReporterRole {
+  Validator = 0,
+  Tracer = 1,
+  Publisher = 2,
+  Authority = 3,
+}
+
+export enum ReporterStatus {
+  Inactive = 0,
+  Active = 1,
+  Unstaking = 2,
+}
