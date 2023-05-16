61dcb0baab5c26ae3cae5fda000c211b6851569f
Andrei Arsenin <andrei.arsenin@hapi.one>
2023-05-16 13:54:05 +0300

    Update contract title

--- evm/contracts/HapiCore.sol
+++ evm/contracts/HapiCore.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
- * @title HAPI Core
+ * @title HAPI Core EVM
  * @author HAPI Protocol development team
  *
  * Core contract for the HAPI protocol
