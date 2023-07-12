12276e6b4bb5b66184a24e4fe7148d9262e24caa
Sergei2404 <serhii.rudniev@hapi.one>
2023-07-12 11:32:58 +0300

    HAPI-398

--- near/contract/src/reward/mod.rs
+++ near/contract/src/reward/mod.rs
@@ -1,18 +1,34 @@
 use near_sdk::{
     borsh::{self, BorshDeserialize, BorshSerialize},
+    env,
+    serde::{Deserialize, Serialize},
     AccountId, Balance,
 };
 
 mod v_reward;
 
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
 pub struct RewardAmounts {
     address_confirmation: Balance,
     address_trace: Balance,
 }
 
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
 pub struct RewardConfiguration {
     token: AccountId,
     reward_amounts: RewardAmounts,
+}
+
+impl Default for RewardConfiguration {
+    fn default() -> Self {
+        Self {
+            token: env::current_account_id(),
+            reward_amounts: RewardAmounts {
+                address_confirmation: Balance::default(),
+                address_trace: Balance::default(),
+            },
+        }
+    }
 }
--- near/contract/src/stake/mod.rs
+++ near/contract/src/stake/mod.rs
@@ -1,11 +1,14 @@
 use near_sdk::{
     borsh::{self, BorshDeserialize, BorshSerialize},
+    env,
+    serde::{Deserialize, Serialize},
     AccountId, Balance, Timestamp,
 };
 
 mod v_stake;
 
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
 pub struct StakeAmounts {
     validator: Balance,
     tracer: Balance,
@@ -13,9 +16,25 @@ pub struct StakeAmounts {
     authority: Balance,
 }
 
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
 pub struct StakeConfiguration {
     token: AccountId,
     unlock_duration: Timestamp,
     stake_amounts: StakeAmounts,
+}
+
+impl Default for StakeConfiguration {
+    fn default() -> Self {
+        Self {
+            token: env::current_account_id(),
+            unlock_duration: Timestamp::default(),
+            stake_amounts: StakeAmounts {
+                validator: Balance::default(),
+                tracer: Balance::default(),
+                publisher: Balance::default(),
+                authority: Balance::default(),
+            },
+        }
+    }
 }
