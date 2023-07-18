863e187dc83e664cf0bab6a0c37911550bc60bb1
Andrei Arsenin <andrei.arsenin@hapi.one>
2023-07-18 13:46:25 +0300

    Add gas price and deploy timeout customization to hapi-core-evm

--- evm/hardhat.config.ts
+++ evm/hardhat.config.ts
@@ -3,7 +3,8 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 
 const HARDHAT_NETWORK = process.env.HARDHAT_NETWORK || "hardhat";
-const HARDHAT_LOCALHOST_URL = process.env.HARDHAT_LOCALHOST_URL || "http://127.0.0.1:8545";
+const HARDHAT_LOCALHOST_URL =
+  process.env.HARDHAT_LOCALHOST_URL || "http://127.0.0.1:8545";
 
 const config: HardhatUserConfig = {
   defaultNetwork: HARDHAT_NETWORK,
@@ -29,7 +30,10 @@ const config: HardhatUserConfig = {
     linea: {
       url: `https://linea-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
       chainId: 59144,
-    }
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : undefined,
+      gasPrice: 3_000_000_000,
+      timeout: 36000,
+    },
   },
   gasReporter: {
     currency: "ETH",
@@ -37,6 +41,15 @@ const config: HardhatUserConfig = {
     enabled: true,
   },
 };
+
+function forceSignerGasPrice(hre: any, signer: any, gwei: string) {
+  signer.getFeeData = async () => ({
+    lastBaseFeePerGas: null,
+    gasPrice: hre.ethers.utils.parseUnits(gwei, "gwei"),
+    maxFeePerGas: null,
+    maxPriorityFeePerGas: null,
+  });
+}
 
 task("deploy", "Deploys the HAPI Core contract").setAction(async (_, hre) => {
   try {
@@ -56,8 +69,16 @@ task("deploy", "Deploys the HAPI Core contract").setAction(async (_, hre) => {
 
     const HapiCore = await hre.ethers.getContractFactory("HapiCore", wallet);
 
+    const gasPrice = process.env.GAS_PRICE_GWEI;
+    if (gasPrice) {
+      console.log(`Enforcing gas price: ${gasPrice} Gwei`);
+      forceSignerGasPrice(hre, HapiCore.signer, gasPrice);
+    }
+
     const contract = await hre.upgrades.deployProxy(HapiCore, [], {
       initializer: "initialize",
+      pollingInterval: Number(process.env.DEPLOY_POLLING_INTERVAL) || 10000,
+      timeout: Number(process.env.DEPLOY_TIMEOUT) || 3600000,
     });
 
     await contract.deployed();
@@ -98,6 +119,12 @@ task("deploy-test-token", "Deploys the HAPI Test Token contract").setAction(
 
       const TestToken = await hre.ethers.getContractFactory("Token", wallet);
 
+      const gasPrice = process.env.GAS_PRICE_GWEI;
+      if (gasPrice) {
+        console.log(`Enforcing gas price: ${gasPrice} Gwei`);
+        forceSignerGasPrice(hre, TestToken.signer, gasPrice);
+      }
+
       const contract = await TestToken.deploy();
 
       await contract.deployed();
@@ -121,6 +148,12 @@ task("upgrade", "Upgrades the HAPI Core contract")
       console.log(`Deploying to '${network.name}' (${network.chainId})`);
 
       const HapiCore = await hre.ethers.getContractFactory("HapiCore");
+
+      const gasPrice = process.env.GAS_PRICE_GWEI;
+      if (gasPrice) {
+        console.log(`Enforcing gas price: ${gasPrice} Gwei`);
+        forceSignerGasPrice(hre, HapiCore.signer, gasPrice);
+      }
 
       const contract = await hre.upgrades.upgradeProxy(args.address, HapiCore);
 
