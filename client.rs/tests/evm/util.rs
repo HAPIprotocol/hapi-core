6bb187050b41681dc23bcfa9980bac8585d3ea75
Andrei Arsenin <andrei.arsenin@hapi.one>
2023-07-31 12:36:28 +0300

    Update client.rs tests to wait for a node to open the port

--- client.rs/tests/evm/util.rs
+++ client.rs/tests/evm/util.rs
@@ -1,5 +1,6 @@
 use ethers::{types::H160, utils::to_checksum as ethers_to_checksum};
 use regex::Regex;
+use std::{net::TcpStream, thread, time::Duration};
 
 pub fn is_tx_match(value: &serde_json::Value) -> bool {
     Regex::new(r"^0x[0-9a-fA-F]{64}$").unwrap().is_match(
@@ -13,4 +14,20 @@ pub fn is_tx_match(value: &serde_json::Value) -> bool {
 
 pub fn to_checksum(value: &str) -> String {
     ethers_to_checksum(&value.parse::<H160>().expect("invalid address"), None)
+}
+
+pub fn wait_for_port(port: u16) {
+    let addr = format!("localhost:{}", port);
+    loop {
+        thread::sleep(Duration::from_secs(1));
+        match TcpStream::connect(&addr) {
+            Ok(_) => {
+                println!("Successfully connected to the server at {}", &addr);
+                break;
+            }
+            Err(e) => {
+                println!("Failed to connect: {}", e);
+            }
+        }
+    }
 }
