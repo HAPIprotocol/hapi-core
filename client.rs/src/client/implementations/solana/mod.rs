cdae964cfc094948045d59b561bdbb81ce35187a
Ola Lytvynenko <olalytvinenko@gmail.com>
2023-09-01 21:40:35 +0300

    token functional added

--- client.rs/src/client/implementations/solana/mod.rs
+++ client.rs/src/client/implementations/solana/mod.rs
@@ -1,7 +1,9 @@
 mod client;
 // pub mod result;
 mod conversion;
+pub mod token;
 mod utils;
 
-pub use client::{HapiCoreSolana, TokenContractSolana};
+pub use client::HapiCoreSolana;
+pub use token::TokenContractSolana;
 pub use utils::get_network_account;
