a3fbb9a300b9c73f53376f94f4c5a0f37681a778
Sergei2404 <serhii.rudniev@hapi.one>
2024-01-09 13:48:07 +0200

    moved indexer creation logic

--- explorer/src/lib.rs
+++ explorer/src/lib.rs
@@ -2,7 +2,6 @@ pub mod application;
 pub mod configuration;
 pub mod entity;
 pub mod error;
-pub mod indexer;
 pub mod observability;
 pub mod routes;
 pub mod service;
--- explorer/src/service/mutation.rs
+++ explorer/src/service/mutation.rs
@@ -1,4 +1,7 @@
-use crate::entity::FromPayload;
+use chrono::{DateTime, NaiveDateTime, Utc};
+use uuid::Uuid;
+
+use crate::entity::{indexer, FromPayload};
 use {hapi_core::HapiCoreNetwork, sea_orm::*};
 
 pub struct Mutation;
@@ -26,5 +29,22 @@ impl Mutation {
         M: ActiveModelBehavior + FromPayload<T> + std::marker::Send,
     {
         M::from(network, payload).update(db).await
+    }
+
+    pub async fn create_indexer(
+        db: &DbConn,
+        network: HapiCoreNetwork,
+        id: Uuid,
+        timestamp: DateTime<Utc>,
+    ) -> Result<indexer::Model, DbErr> {
+        indexer::ActiveModel {
+            id: Set(id),
+            network: Set(network.into()),
+            created_at: Set(timestamp.naive_utc()),
+            last_heartbeat: Set(NaiveDateTime::default()),
+            cursor: Set("".to_string()),
+        }
+        .insert(db)
+        .await
     }
 }
