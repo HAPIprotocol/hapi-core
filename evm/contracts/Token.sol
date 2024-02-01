5bb40a7301aa4e8bfcb33a6c587a63f99132a588
Andrei Arsenin <andrei.arsenin@hapi.one>
2024-02-01 17:25:41 +0200

    Bump eth compiler to 0.8.24

--- evm/contracts/Token.sol
+++ evm/contracts/Token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
