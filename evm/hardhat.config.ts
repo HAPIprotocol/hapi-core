40e90f0c226d482093cc1a47e1b80b190744c007
Andrei Arsenin <andrei.arsenin@hapi.one>
2024-02-02 15:37:59 +0200

    Add a task for the EVM contract to update stake configuration

--- evm/hardhat.config.ts
+++ evm/hardhat.config.ts
@@ -1,7 +1,11 @@
 import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 import "@openzeppelin/hardhat-upgrades";
 import "@nomicfoundation/hardhat-toolbox";
-import { Contract } from "ethers";
+import { Contract, ContractTransactionResponse } from "ethers";
+
+import { HapiCore, Token } from "./typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 const HARDHAT_NETWORK = process.env.HARDHAT_NETWORK || "hardhat";
 const HARDHAT_LOCALHOST_URL =
@@ -192,6 +196,160 @@ task("upgrade", "Upgrades the HAPI Core contract")
         admin: adminAddress,
         implementation: implementationAddress,
       });
+    } catch (error) {
+      console.error(`${error}`);
+      process.exit(1);
+    }
+  });
+
+async function setup(
+  hre: HardhatRuntimeEnvironment
+): Promise<{ signer: HardhatEthersSigner; token: Token; hapiCore: HapiCore }> {
+  let [signer] = await hre.ethers.getSigners();
+
+  console.log("==> Environment setup");
+
+  if (!signer) {
+    throw new Error("No signer found");
+  } else {
+    console.log(`Signer: ${signer.address}`);
+  }
+
+  const network = await hre.ethers.provider.getNetwork();
+  console.log(`Network: '${network.name}' (${network.chainId})`);
+
+  const coinBalance = await hre.ethers.provider.getBalance(signer.address);
+  console.log(`Balance: ${hre.ethers.formatEther(coinBalance)} ETH`);
+
+  let token: Token;
+  {
+    const tokenAddress = process.env.TOKEN_ADDRESS;
+
+    if (!tokenAddress || !hre.ethers.isAddress(tokenAddress)) {
+      throw new Error("No token address found (use TOKEN_ADDRESS env var)");
+    }
+
+    const tokenFactory = await hre.ethers.getContractFactory("Token");
+    token = tokenFactory.attach(tokenAddress) as Token;
+
+    console.log(`Token: ${tokenAddress}`);
+
+    const [tokenBalance, decimals, symbol] = await Promise.all([
+      token.balanceOf(signer.address),
+      token.decimals(),
+      token.symbol(),
+    ]);
+
+    console.log(
+      `Token balance: ${hre.ethers.formatUnits(
+        tokenBalance,
+        decimals
+      )} ${symbol}`
+    );
+  }
+
+  let hapiCore: HapiCore;
+  {
+    const contractAddress = process.env.CONTRACT_ADDRESS;
+
+    if (!contractAddress || !hre.ethers.isAddress(contractAddress)) {
+      throw new Error(
+        "No contract address found (use CONTRACT_ADDRESS env var)"
+      );
+    }
+
+    const hapiCoreFactory = await hre.ethers.getContractFactory("HapiCore");
+    hapiCore = hapiCoreFactory.attach(contractAddress) as unknown as HapiCore;
+
+    console.log(`HAPI Core: ${contractAddress}`);
+  }
+
+  console.log();
+
+  return { signer, token, hapiCore };
+}
+
+async function trackTransaction(response: ContractTransactionResponse) {
+  console.log("\n==> Executing transaction");
+  console.log(`Transaction: ${response.hash}`);
+  console.log("Waiting for transaction confirmation...");
+  await response.wait();
+  console.log(`Finished with ${await response.confirmations()} confirmation(s)`);
+}
+
+task("update-stake-configuration", "Updates the stake configuration")
+  .addParam("unlockDuration", "Unlock duration")
+  .addParam("validatorStake", "Validator stake")
+  .addParam("tracerStake", "Tracer stake")
+  .addParam("publisherStake", "Publisher stake")
+  .addParam("authorityStake", "Authority stake")
+  .setAction(async (args, hre) => {
+    try {
+      const { hapiCore, token } = await setup(hre);
+
+      console.log("==> Updating stake configuration");
+
+      const [name, symbol, decimals] = await Promise.all([
+        token.name(),
+        token.symbol(),
+        token.decimals(),
+      ]);
+
+      console.log(`Token: ${name} (${symbol})`);
+
+      console.log(`Unlock duration: ${args.unlockDuration} seconds`);
+
+      const validatorStake = hre.ethers.parseUnits(
+        args.validatorStake,
+        decimals
+      );
+      console.log(
+        `Validator stake: ${hre.ethers.formatUnits(
+          validatorStake,
+          decimals
+        )} ${symbol}`
+      );
+
+      const tracerStake = hre.ethers.parseUnits(args.tracerStake, decimals);
+      console.log(
+        `Tracer stake: ${hre.ethers.formatUnits(
+          tracerStake,
+          decimals
+        )} ${symbol}`
+      );
+
+      const publisherStake = hre.ethers.parseUnits(
+        args.publisherStake,
+        decimals
+      );
+      console.log(
+        `Publisher stake: ${hre.ethers.formatUnits(
+          publisherStake,
+          decimals
+        )} ${symbol}`
+      );
+
+      const authorityStake = hre.ethers.parseUnits(
+        args.authorityStake,
+        decimals
+      );
+      console.log(
+        `Authority stake: ${hre.ethers.formatUnits(
+          authorityStake,
+          decimals
+        )} ${symbol}`
+      );
+
+      const response = await hapiCore.updateStakeConfiguration(
+        await token.getAddress(),
+        args.unlockDuration,
+        validatorStake,
+        tracerStake,
+        publisherStake,
+        authorityStake
+      );
+
+      await trackTransaction(response);
     } catch (error) {
       console.error(`${error}`);
       process.exit(1);
